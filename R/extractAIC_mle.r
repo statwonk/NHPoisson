c5b11e07d199752fcf97b8f400814a56f253655a
Christopher Peters <cpeter9@gmail.com>
2016-04-12 19:34:43 -0500

    Improve indentation for readability

--- R/extractAIC_mle.r
+++ R/extractAIC_mle.r
@@ -1,10 +1,10 @@
 setMethod("extractAIC",
-    signature(fit = "mle"),
-    function (fit, scale, k = 2, ...) 
-    {
- 	   res <- logLik(fit)
- 	   edf <- attr(res, "df")
-	    c(edf, -2 * res + k * edf)
-    }
+          signature(fit = "mle"),
+          function (fit, scale, k = 2, ...)
+          {
+            res <- logLik(fit)
+            edf <- attr(res, "df")
+            c(edf, -2 * res + k * edf)
+          }
 
 )
